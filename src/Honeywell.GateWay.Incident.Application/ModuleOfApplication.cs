4acb101810eb9a199cc5b26067853868746036aa
Baron Chen <Baron.Chen2@honeywell.com>
2019-09-17 11:17:49 +0800

    add the IocContainer remote services

--- src/Honeywell.GateWay.Incident.Application/ModuleOfApplication.cs
+++ src/Honeywell.GateWay.Incident.Application/ModuleOfApplication.cs
@@ -3,6 +3,7 @@ using Honeywell.Infra.Core.Modular;
 using Honeywell.Infra.HoneyMapper.AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 using Honeywell.Infra.Client.WebApi;
+using Honeywell.Micro.Services.Incident.Api;
 using Honeywell.Micro.Services.Workflow.Api;
 using Microsoft.Extensions.Configuration;
 
@@ -22,6 +23,10 @@ namespace Honeywell.GateWay.Incident.Application
             IocContainer.AddScoped<IIncidentAppService, IncidentAppService>();
             IocContainer.AddRemoteService(
                 typeof(IWorkflowDesignApi).Assembly);
+            IocContainer.AddRemoteService(
+                typeof(IIncidentMicroApi).Assembly);
+            IocContainer.AddRemoteService(
+                typeof(IWorkflowInstanceApi).Assembly);
 
 
             // for stub
