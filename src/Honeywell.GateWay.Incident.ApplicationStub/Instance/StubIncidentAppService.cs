a8cd49f686a07b15fbfdb827a6bd15a8794a1933
Baron Chen <Baron.Chen2@honeywell.com>
2019-09-18 17:14:41 +0800

    add stub for gateways services

---
+++ src/Honeywell.GateWay.Incident.ApplicationStub/Instance/StubIncidentAppService.cs
@@ -0,0 +1,42 @@
+﻿using Honeywell.Micro.Services.Incident.Api;
+using Honeywell.Micro.Services.Incident.Api.Incident.Create;
+using Honeywell.Micro.Services.Incident.Api.Incident.Details;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Honeywell.Micro.Services.Incident.Domain.Shared;
+
+namespace Honeywell.GateWay.Incident.ApplicationStub.Instance
+{
+    class StubIncidentAppService : IIncidentMicroApi
+    {
+        public Task<CreateIncidentResponseDto> Create(CreateIncidentRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetIncidentDetailsResponseDto> GetDetails(GetIncidentDetailsRequestDto request)
+        {
+            var detail = new IncidentDto()
+            {
+                Id = Guid.NewGuid(),
+                Description = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a" +
+                "horrible vermin.He lay on his armour - like back," +
+                "and if he lifted his head a little he could see his brown belly",
+                Number = 137,
+                CreateAtUtc = DateTime.UtcNow,
+                LastUpdateAtUtc = DateTime.UtcNow,
+                Owner = "Admin1",
+                Priority = IncidentPriority.High,
+                State = IncidentState.Active,
+                WorkflowId = Guid.NewGuid(),
+            };
+            var response = new GetIncidentDetailsResponseDto()
+            {
+                IsSuccess =  true,
+                Details = new List<IncidentDto>() { detail }
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ src/Honeywell.GateWay.Incident.ApplicationStub/Workflow/StubWorkflowApiService.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Honeywell.Micro.Services.Workflow.Api;
+using Honeywell.Micro.Services.Workflow.Api.Workflow.Create;
+using Honeywell.Micro.Services.Workflow.Api.Workflow.Details;
+using Honeywell.Micro.Services.Workflow.Api.Workflow.List;
+using Honeywell.Micro.Services.Workflow.Api.Workflow.Summary;
+using Honeywell.Micro.Services.Workflow.Domain.Shared;
+
+namespace Honeywell.GateWay.Incident.ApplicationStub.Workflow
+{
+    public class StubWorkflowApiService : IWorkflowInstanceApi
+    {
+        public Task<CreateWorkflowInstanceResponseDto> Create(CreateWorkflowInstanceRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus CompleteWorkflow(Guid workflowId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus CancelWorkflow(Guid workflowId, string reason)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WorkflowSummaryDto[] GetWorkflowSummaries(Guid[] workflowIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus AddComment(Guid workflowId, string comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus AddStepComment(Guid workflowId, Guid workflowStepId, string comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus AcknowledgeActivity(Guid workflowId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus RemoveOwner(Guid workflowId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus TakeOwnership(Guid workflowId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActivityStatus UpdateWorkflowStepStatus(Guid workflowId, Guid workflowStepId, bool isComplete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WorkflowInstanceDto[] GetWorkflows(Guid[] workflowIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WorkflowDetailsResponseDto> GetWorkflowDetails(WorkflowDetailsRequestDto request)
+        {
+
+            var step1 = new WorkflowStepDto()
+            {
+                Id = Guid.NewGuid(),
+                IsComplete = false,
+                IsOptional = true,
+                Instruction = "Curzon Hall Lobby Court West external Gates can be unlocked remotely.",
+                HelpText = "3 gates labelled as: Door 2, 3, 4."
+            };
+
+            var step2 = new WorkflowStepDto()
+            {
+                Id = Guid.NewGuid(),
+                IsComplete = true,
+                IsOptional = false,
+                Instruction = "Curzon Hall Lobby staircase pressurisation can be triggered from fire alarm.",
+                HelpText = "Must inform control room if triggering fire alarm."
+            };
+
+
+            var step3 = new WorkflowStepDto()
+            {
+                Id = Guid.NewGuid(),
+                IsComplete = false,
+                IsOptional = true,
+                Instruction = "Confirm with event manager on final event requirements and special needs.  Provide link to event preparation contract.",
+                HelpText = "View event booking calendar. On the calendar, link to event contract."
+            };
+
+
+            var step4 = new WorkflowStepDto()
+            {
+                Id = Guid.NewGuid(),
+                IsComplete = false,
+                IsOptional = false,
+                Instruction = "If required by the event, engage Energy team for pre-event check.",
+                HelpText = "Any cabling on special electricity supply? Backup generators ready?"
+            };
+
+            var step5 = new WorkflowStepDto()
+            {
+                Id = Guid.NewGuid(),
+                IsComplete = true,
+                IsOptional = true,
+                Instruction = "Threat assessment has been performed.",
+                HelpText = "Link to security plan for the event."
+            };
+
+
+            var detail = new WorkflowDto()
+            {
+                Id = Guid.NewGuid(),
+                Owner = "admin1",
+                Status = WorkflowStatus.Active,
+                Number = 137,
+                Version = 12,
+                Name = "Event Prep Master",
+                Description = "This procedure shall be completed 48hours before any events.  This procedure is for Convention Centre buildings.",
+                WorkflowSteps = new[] { step1, step2, step3, step4, step5 }
+            };
+
+
+            var response = new WorkflowDetailsResponseDto()
+            {
+                IsSuccess =  true,
+                Details =  new List<WorkflowDto> { detail }
+            };
+            return Task.FromResult(response);
+        }
+
+        public WorkflowInstanceDto[] GetPermissions(WorkflowInstanceDto[] workflows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
