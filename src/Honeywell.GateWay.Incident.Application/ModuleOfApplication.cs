a5c3f1b205486cf703552481868b020c133edb12
Baoyi, Liang <baoyi.liang@honeywell.com>
2019-09-18 19:44:35 +0800

    fix bug

--- src/Honeywell.GateWay.Incident.Application/ModuleOfApplication.cs
+++ src/Honeywell.GateWay.Incident.Application/ModuleOfApplication.cs
@@ -1,8 +1,10 @@
-﻿using Honeywell.GateWay.Incident.Application.Incident;
+﻿using Honeywell.Facade.Services.Incident.Api;
+using Honeywell.GateWay.Incident.Application.Incident;
 using Honeywell.Infra.Core.Modular;
 using Honeywell.Infra.HoneyMapper.AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 using Honeywell.Infra.Client.WebApi;
+using Honeywell.Micro.Services.Incident.Api;
 using Honeywell.Micro.Services.Workflow.Api;
 using Microsoft.Extensions.Configuration;
 
@@ -20,9 +22,10 @@ namespace Honeywell.GateWay.Incident.Application
             });
 
             IocContainer.AddScoped<IIncidentAppService, IncidentAppService>();
-            IocContainer.AddRemoteService(
-                typeof(IWorkflowDesignApi).Assembly);
-
+            IocContainer.AddRemoteService(typeof(IWorkflowDesignApi).Assembly);
+            IocContainer.AddRemoteService(typeof(IWorkflowInstanceApi).Assembly);
+            IocContainer.AddRemoteService(typeof(IIncidentMicroApi).Assembly);
+            IocContainer.AddRemoteService(typeof(IIncidentFacadeApi).Assembly);
 
             // for stub
             //IocContainer.AddScoped<IWorkflowDesignApi, StubWorkflowDesignAppService>();
