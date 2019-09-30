48eb9b1b94aa2fa32c9aeb1cbb23e708083724e9
Baron Chen <Baron.Chen2@honeywell.com>
2019-09-30 16:59:30 +0800

    add run api test scripts

--- test/api/Incident.ApiTests/RunApiTest.ps1
+++ test/api/Incident.ApiTests/RunApiTest.ps1
@@ -1,1 +1,24 @@
-dotnet vstest Incident.ApiTests.dll
+param(
+    [string]$MailtoAddress,
+	[string]$MailSubject,
+	[string]$Mailbody,
+	[string]$AttachmentFile,
+	[string]$MailPriority,
+	[string]$URL,
+    [string]$UserName,
+    [string]$Password
+)
+
+$resultContent =  dotnet vstest Incident.ApiTests.dll
+Set-Content -Path .\TestResult.txt -Value $resultContent
+#send mail
+if($resultContent -like "*Failed*"){
+    ..\..\assets\SendEmail.ps1 -MailtoAddress "$MailtoAddress" -MailSubject "Incident Gateway API test failed on $URL" -MailPriority "High" -AttachmentFile .\TestResult.txt
+    exit 0
+}
+else{
+    ..\..\assets\SendEmail.ps1 -MailtoAddress "$MailtoAddress" -MailSubject "Incident Gateway API test passed on $URL" -MailPriority "Normal" -AttachmentFile .\TestResult.txt
+    exit 1
+}
+
+
