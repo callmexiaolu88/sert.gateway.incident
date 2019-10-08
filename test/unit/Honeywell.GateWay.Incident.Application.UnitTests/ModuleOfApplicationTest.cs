d61a1ee500999f877ff6f483d06930fed615bc1a
Baron Chen <Baron.Chen2@honeywell.com>
2019-10-08 16:38:23 +0800

    add module of app service ut cases

---
+++ test/unit/Honeywell.GateWay.Incident.Application.UnitTests/ModuleOfApplicationTest.cs
@@ -0,0 +1,20 @@
+﻿using Honeywell.GateWay.Incident.Application.Incident;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Honeywell.GateWay.Incident.Application.UnitTests
+{
+    public class ModuleOfApplicationTest
+    {
+        [Fact]
+        public void TestModuleOfApp()
+        {
+            var serviceCollection = new ServiceCollection();
+            var moduleOfApplication = new ModuleOfApplication(serviceCollection);
+            moduleOfApplication.InitializeDependencyInject();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var incidentService = serviceProvider.GetService<IIncidentAppService>();
+            Assert.NotNull(incidentService);
+        }
+    }
+}
