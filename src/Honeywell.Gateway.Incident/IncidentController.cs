543b1a1ce4548009dee128788c53a50446c09133
Char Zhao <zhaojun20002003@hotmail.com>
2019-10-10 09:02:06 +0800

    fix httppost issue

--- src/Honeywell.Gateway.Incident/IncidentController.cs
+++ src/Honeywell.Gateway.Incident/IncidentController.cs
@@ -106,6 +106,7 @@ namespace Honeywell.Gateway.Incident
             return activeIncidents;
         }
 
+        [HttpPost]
         public async Task<DeviceGto[]> GetDevices()
         {
             var devices = await _incidentAppService.GetDevices();
