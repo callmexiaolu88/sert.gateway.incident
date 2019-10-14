a73c49bdab0f997c2788a083faa0a724fcdebfde
xiaoboliu <2239940721@qq.com>
2019-10-14 17:38:08 +0800

    ISRT-585 update

--- src/Honeywell.GateWay.Incident.Application/Incident/IncidentAppService.cs
+++ src/Honeywell.GateWay.Incident.Application/Incident/IncidentAppService.cs
@@ -136,6 +136,7 @@ namespace Honeywell.GateWay.Incident.Application.Incident
         {
             return await _incidentRepository.CloseIncident(incidentId, reason);
         }
+
         public async Task<ExecuteResult> CompleteIncident(string incidentId)
         {
             return await _incidentRepository.CompleteIncident(incidentId);
