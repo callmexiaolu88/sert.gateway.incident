26764a04f2d90a97f82859ca5be98a4c087cce5a
Liu <Xiaobo.Liu@Honeywell.com>
2019-10-22 08:53:09 +0800

    ISRT-585 add  taleover and complete incident apitest

---
+++ test/api/Incident.ApiTests/IncidentControllerTest/IncidentActionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Honeywell.Gateway.Incident.Api.Gtos;
+using Xunit;
+
+namespace Incident.ApiTests.IncidentControllerTest
+{
+    [Collection(Colections.DICollection)]
+    public class IncidentActionTest : BaseIncIdentControllerTest
+    {
+        public IncidentActionTest(DIFixture dIFixture) : base(dIFixture)
+        {
+
+        }
+
+        [Fact]
+        public async void IncidentAction_Success()
+        {
+            await ImportWorkflowDesign();
+            var incidentId = CreateIncident().Result;
+
+            var takeoverResult = await IncidentGateWayApi.TakeoverIncident(incidentId);
+            Assert.True(takeoverResult.Status == ExecuteStatus.Successful);
+
+            var completeResult = await IncidentGateWayApi.CompleteIncident(incidentId);
+            Assert.True(completeResult.Status == ExecuteStatus.Successful);
+
+            await DeleteIncident(incidentId);
+            await DeleteWorkflowDesign();
+        }
+    }
+}
