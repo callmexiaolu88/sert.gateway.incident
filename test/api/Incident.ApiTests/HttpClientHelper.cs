fe35785ab4e9fcc8a775ebcdfab56197f4429a83
H270554 <Kuo.Chen@honeywell.com>
2019-10-22 17:27:27 +0800

    modify code

--- test/api/Incident.ApiTests/HttpClientHelper.cs
+++ test/api/Incident.ApiTests/HttpClientHelper.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using static Incident.ApiTests.ProWatchClient;
 
 namespace Incident.ApiTests
 {
-   public class HttpClientHelper
+    public class HttpClientHelper
     {
         private readonly HttpClient _client;
-        private readonly string _address;
+        private string _address;
+        private string _host;
+        private string _username;
+        private string _password;
+
         public HttpClientHelper()
         {
             _client = new HttpClient(new HttpClientHandler
@@ -19,14 +26,11 @@ namespace Incident.ApiTests
                 ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true,
             });
 
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            config.Providers.First().TryGet("RemoteServicesConfig:DefaultUrl", out _address);
+            InitParameterData();
         }
 
-        public async Task<HttpResponseMessage> PostAsync(string apiUri, HttpContent content, Dictionary<string, string> headers)
+        public async Task<HttpResponseMessage> PostAsync(string apiUri, HttpContent content,
+            Dictionary<string, string> headers)
         {
             _client.DefaultRequestHeaders.Clear();
             foreach (var header in headers)
@@ -40,20 +44,56 @@ namespace Incident.ApiTests
             {
                 throw new Exception($"access {apiUri} execute error.");
             }
+
             return response;
         }
 
         public Dictionary<string, string> InitHeader()
         {
-            var tokenHeader = $"Bearer {Token}";
+            var tokenHeader = $"Bearer {GetToken()}";
             var headers = new Dictionary<string, string>
             {
-                { "Authorization", tokenHeader }, { "Accept-Language", "en-us" }
+                {"Authorization", tokenHeader}, {"Accept-Language", "en-us"}
             };
             return headers;
         }
 
-        private const string Token =
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlVkd2xoSF9OZVU2LUtOS3BGOHNpbUFqeExubyIsImtpZCI6IlVkd2xoSF9OZVU2LUtOS3BGOHNpbUFqeExubyJ9.eyJpc3MiOiJodHRwczovL2MyZmFjeWhmYW9ia2Rrdi9UaGlua3RlY3R1cmVJZGVudGl0eVNlcnZpY2UiLCJhdWQiOiJodHRwczovL2MyZmFjeWhmYW9ia2Rrdi9UaGlua3RlY3R1cmVJZGVudGl0eVNlcnZpY2UvcmVzb3VyY2VzIiwiZXhwIjoxNTY5NTgyOTQ2LCJuYmYiOjE1Njk1NDY5NDYsImNsaWVudF9pZCI6IlByb1dhdGNoV2ViQ2xpZW50Iiwic2NvcGUiOlsib2ZmbGluZV9hY2Nlc3MiLCJvcGVuaWQiLCJwcm93YXRjaCJdLCJzdWIiOiJBZG1pbmlzdHJhdG9yIiwiYXV0aF90aW1lIjoxNTY5NTQ2OTQ2LCJpZHAiOiJpZHNydiIsImV4dGVybmFsX3Byb3ZpZGVyX3VzZXJfaWQiOiJBZG1pbmlzdHJhdG9yIiwiYW1yIjpbInBhc3N3b3JkIl19.peYihBbr6T5Fnn12JjjIdgiQ0UuJB45kSQLEzlGHNiVWokrn0jFSJ44yIC5xubo8h8r-R17rmU2LJrnh7X8Myb0-a71NFcC8uqShNrR4NNRM1RrCv6RJBzeDuwGjrMEyoKsDimQuHu54M1HJ-vUvatdQzqQEN6ULqIfAD0sZXktPFxMXlbMUfgqLws5NshTyo937n3_wmadxF2Q83kQgUA_R4Aiz9JoE_radBU6L6JdR43DKCR2LAAHUOEN6fAN6k2kjaNtzinYvcghvIBK1r_ujx65qJ6PWpsvYFMOwNgW4Qws4kGq_2uk1j8st_gxkcRwo67YzH_rtYFwvI-lkAw";
+        private void InitParameterData()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            config.Providers.First().TryGet("RemoteServicesConfig:DefaultUrl", out _address);
+            config.Providers.First().TryGet("ProWatchIsomUrl", out _host);
+            config.Providers.First().TryGet("UserName", out _username);
+            config.Providers.First().TryGet("Password", out _password);
+        }
+
+        private string GetToken()
+        {
+            var tokenString = JsonConvert.DeserializeObject<TokenResult>(RequestTokenAsync(_host, _username, _password).Result);
+            return tokenString.AccessToken;
+        }
+        private async Task<string> RequestTokenAsync(string host, string username, string password)
+        {
+            var aaUrl = new Uri($"{host}/AuthenticationAuthorizationService/api/AuthenticationAuthorization/AuthenticateClientRequest");
+            var bodyContent = MockDefaultHttpContent(username, password);
+            var content = new StringContent(JsonConvert.SerializeObject(bodyContent), Encoding.UTF8, "application/json");
+            var response = _client.PostAsync(aaUrl, content).Result;
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
+        private RequestContent MockDefaultHttpContent(string username, string password)
+        {
+            var content = new RequestContent
+            {
+                Username = username,
+                Password = password,
+                workspace = "",
+                ClientID = "ProWatchWebClient",
+                ClientSecret = "secret",
+                Scope = "prowatch offline_access"
+            };
+            return content;
+        }
     }
 }
