89dbaf26b89448a344484d3d40def2d69c97c38c
H270554 <Kuo.Chen@honeywell.com>
2019-10-23 11:58:32 +0800

    modify api test

--- test/api/Incident.ApiTests/IncidentControllerTest/AddStepCommentTest.cs
+++ test/api/Incident.ApiTests/IncidentControllerTest/AddStepCommentTest.cs
@@ -1,4 +1,6 @@
-﻿using Honeywell.Gateway.Incident.Api.Gtos;
+﻿using System;
+using System.Linq;
+using Honeywell.Gateway.Incident.Api.Gtos;
 using Xunit;
 
 namespace Incident.ApiTests.IncidentControllerTest
@@ -19,14 +21,19 @@ namespace Incident.ApiTests.IncidentControllerTest
 
             var incidentDetails = await IncidentGateWayApi.GetIncidentById(incidentId);
             var workflowStepId = incidentDetails.IncidentSteps[0].Id;
+            string commentRemark = Guid.NewGuid().ToString();
             var addStepCommentGto = new AddStepCommentGto()
             {
                 WorkflowStepId = workflowStepId.ToString(),
-                Comment = "this is comment."
+                Comment = $"this is comment.|{commentRemark}"
             };
             var result = await IncidentGateWayApi.AddStepComment(addStepCommentGto);
-
             Assert.True(result.Status == ExecuteStatus.Successful);
+
+            var incidentDetailReponse = await IncidentGateWayApi.GetIncidentById(incidentId);
+            bool isAddCommentSucess = incidentDetailReponse.IncidentSteps.First(o => o.Id == workflowStepId)
+                .StepComments.Any(x => x.Description.Contains(commentRemark));
+            Assert.True(isAddCommentSucess);
 
             await DeleteIncident(incidentId);
             await DeleteWorkflowDesign();
