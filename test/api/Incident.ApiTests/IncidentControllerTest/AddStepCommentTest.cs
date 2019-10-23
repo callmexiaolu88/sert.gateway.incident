a1ca06ec913fac98d1ef8efeef58e423a686c707
H270554 <Kuo.Chen@honeywell.com>
2019-10-23 11:46:06 +0800

    add api test

---
+++ test/api/Incident.ApiTests/IncidentControllerTest/AddStepCommentTest.cs
@@ -0,0 +1,35 @@
+﻿using Honeywell.Gateway.Incident.Api.Gtos;
+using Xunit;
+
+namespace Incident.ApiTests.IncidentControllerTest
+{
+    [Collection(Colections.DICollection)]
+    public class AddStepCommentTest : BaseIncIdentControllerTest
+    {
+        public AddStepCommentTest(DIFixture dIFixture) : base(dIFixture)
+        {
+
+        }
+
+        [Fact]
+        public async void AddStepComment_Success()
+        {
+            await ImportWorkflowDesign();
+            var incidentId = CreateIncident().Result;
+
+            var incidentDetails = await IncidentGateWayApi.GetIncidentById(incidentId);
+            var workflowStepId = incidentDetails.IncidentSteps[0].Id;
+            var addStepCommentGto = new AddStepCommentGto()
+            {
+                WorkflowStepId = workflowStepId.ToString(),
+                Comment = "this is comment."
+            };
+            var result = await IncidentGateWayApi.AddStepComment(addStepCommentGto);
+
+            Assert.True(result.Status == ExecuteStatus.Successful);
+
+            await DeleteIncident(incidentId);
+            await DeleteWorkflowDesign();
+        }
+    }
+}
