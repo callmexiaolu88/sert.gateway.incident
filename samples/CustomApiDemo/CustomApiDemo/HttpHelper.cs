9e2ea82bc2ddea9627d4012f339851169ba508ab
Lu <Yulong.Lu@Honeywell.com>
2019-10-31 11:27:45 +0800

    modify custom api

--- samples/CustomApiDemo/CustomApiDemo/HttpHelper.cs
+++ samples/CustomApiDemo/CustomApiDemo/HttpHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -8,40 +10,82 @@ namespace CustomApiDemo
 {
     public class HttpHelper
     {
-        private const string RemoteServicesUri = "https://N0QTT7LWUNF7CYJ/webapi/";
+        private ProWatchClient _proWatchClient;
 
-        private const string Token =
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlVkd2xoSF9OZVU2LUtOS3BGOHNpbUFqeExubyIsImtpZCI6IlVkd2xoSF9OZVU2LUtOS3BGOHNpbUFqeExubyJ9.eyJpc3MiOiJodHRwczovL2RoOGJjZ3VhamF6MjJ5MC9UaGlua3RlY3R1cmVJZGVudGl0eVNlcnZpY2UiLCJhdWQiOiJodHRwczovL2RoOGJjZ3VhamF6MjJ5MC9UaGlua3RlY3R1cmVJZGVudGl0eVNlcnZpY2UvcmVzb3VyY2VzIiwiZXhwIjoxNTcyNDMzMDQxLCJuYmYiOjE1NzIzOTcwNDEsImNsaWVudF9pZCI6IlByb1dhdGNoV2ViQ2xpZW50Iiwic2NvcGUiOlsib2ZmbGluZV9hY2Nlc3MiLCJvcGVuaWQiLCJwcm93YXRjaCJdLCJzdWIiOiJhZG1pbmlzdHJhdG9yIiwiYXV0aF90aW1lIjoxNTcyMzk3MDQxLCJpZHAiOiJpZHNydiIsImV4dGVybmFsX3Byb3ZpZGVyX3VzZXJfaWQiOiJhZG1pbmlzdHJhdG9yIiwicm9sZSI6IkFkbWluaXN0cmF0b3IiLCJhbXIiOlsicGFzc3dvcmQiXX0.TGCnPmGolbnqBis27Ndo-_f-kYEUEhjtAsKNS-jU4Fr0IgCiR-_-j1ZD6Fr5oflj0c8A4-fC-LkDehlozeIGE_6Fy6l9I4pETSGlzvJgQAzHCm2sgp9RmOIpWV4_lT5CmKFXfq4SyHylCYeU2D5gNRWRfAQNBhAMK13ydUrtIesiT4_b_SauzQtJ3uzKdlU_EnwtN9M-pxGKiAxSsWelHUkGCLlPQqQP_d9nXd3hkfFVuIHZxIlADZEdwNUFxCv7-ZYs9IYXCnSgNHts7EZDFMEf_xGqgnzujUs3bEu3NVVPwVLPRn6jgjDbbYOQ0PRqO_6kJ0ybk3ZXJb860G9geA";
-        private static HttpClient CreateHttpClient()
+        static HttpHelper()
         {
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-            var client = new HttpClient
-            {
-                BaseAddress = new Uri(RemoteServicesUri)
-            };
+        }
+
+        public HttpHelper()
+        {
+            _proWatchClient = new ProWatchClient(this);
+        }
+
+        public static HttpClient CreateHttpClient()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.AcceptLanguage.Add(
+                new StringWithQualityHeaderValue(ConfigurationManager.AppSettings["DefaultLanguage"]));
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
             return client;
         }
 
-        public Task<TResult> PostAsync<T, TResult>(string requestUri, T value)
+        public async Task<TResult> PostAsync<T, TResult>(string requestUri, T value)
+        {
+            var client = CreateHttpClient();
+            client.DefaultRequestHeaders.Authorization = _proWatchClient.GenerateTokenAsync();
+            var httpResponse = client.PostAsJsonAsync(generatorIncidentUri(requestUri), value);
+            httpResponse.Result.EnsureSuccessStatusCode();
+            var result = await httpResponse.Result.Content.ReadAsAsync<TResult>();
+            client.Dispose();
+            return result;
+        }
+
+        public async Task<TResult> PostAsync<TResult>(string requestUri)
+        {
+            var client = CreateHttpClient();
+            client.DefaultRequestHeaders.Authorization = _proWatchClient.GenerateTokenAsync();
+            var httpResponse = client.PostAsync(generatorIncidentUri(requestUri), null);
+            httpResponse.Result.EnsureSuccessStatusCode();
+            var result = await httpResponse.Result.Content.ReadAsAsync<TResult>();
+            client.Dispose();
+            return result;
+        }
+
+        public async Task<TResult> ProWatchPostAsync<T, TResult>(string requestUri, T value)
         {
             var client = CreateHttpClient();
             var httpResponse = client.PostAsJsonAsync(requestUri, value);
             httpResponse.Result.EnsureSuccessStatusCode();
-            var result = httpResponse.Result.Content.ReadAsAsync<TResult>();
+            var result = await httpResponse.Result.Content.ReadAsAsync<TResult>();
+            client.Dispose();
             return result;
         }
 
-        public Task<TResult> PostAsync<TResult>(string requestUri)
+        private Uri generatorIncidentUri(string requestUri)
         {
-            var client = CreateHttpClient();
-            var httpResponse = client.PostAsync(requestUri, null);
-            httpResponse.Result.EnsureSuccessStatusCode();
-            var result = httpResponse.Result.Content.ReadAsAsync<TResult>();
-            return result;
+            string baseUri = ConfigurationManager.AppSettings["SercurityConsoleUrl"];
+            var uriBuilder = new UriBuilder(baseUri);
+            if (uriBuilder.Path.EndsWith("/"))
+            {
+                if (requestUri.StartsWith("/"))
+                {
+                    requestUri = requestUri.Substring(1);
+                }
+            }
+            else
+            {
+                if (!requestUri.StartsWith("/"))
+                {
+                    requestUri = "/" + requestUri;
+                }
+            }
+
+            uriBuilder.Path = uriBuilder.Path + requestUri;
+            return uriBuilder.Uri;
         }
     }
 }
