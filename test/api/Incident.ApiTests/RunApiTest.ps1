0b6ac2ac3f92c52d7969cbd28eb6b4e88e1b224c
Char Zhao <zhaojun20002003@hotmail.com>
2019-12-20 11:26:49 +0800

    update api test mail subject

--- test/api/Incident.ApiTests/RunApiTest.ps1
+++ test/api/Incident.ApiTests/RunApiTest.ps1
@@ -14,11 +14,11 @@ $resultContent =  dotnet vstest Incident.ApiTests.dll
 Set-Content -Path $AttachmentFile -Value $resultContent
 #send mail
 if($resultContent -like "*Test Run Successful*"){
-    ..\..\assets\SendEmail.ps1 -MailtoAddress "$MailtoAddress" -MailSubject "Incident Gateway API test Successful on $URL" -MailPriority "Normal" -AttachmentFile "$AttachmentFile"
+    ..\..\assets\SendEmail.ps1 -MailtoAddress "$MailtoAddress" -MailSubject "Gateway---Incident API test Successful on $URL" -MailPriority "Normal" -AttachmentFile "$AttachmentFile"
     exit 0
 }
 else{
-    ..\..\assets\SendEmail.ps1 -MailtoAddress "$MailtoAddress" -MailSubject "Incident Gateway API test failed on $URL" -MailPriority "High" -AttachmentFile "$AttachmentFile"
+    ..\..\assets\SendEmail.ps1 -MailtoAddress "$MailtoAddress" -MailSubject "Gateway---Incident API test failed on $URL" -MailPriority "High" -AttachmentFile "$AttachmentFile"
     exit 1
 }
 
