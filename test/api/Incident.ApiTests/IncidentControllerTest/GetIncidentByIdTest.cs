2511cb2b1e433bee94b56ad22dccc2959c7e6b4d
Baron Chen <Baron.Chen2@honeywell.com>
2020-01-15 14:27:56 +0800

    add get details with camera information

--- test/api/Incident.ApiTests/IncidentControllerTest/GetIncidentByIdTest.cs
+++ test/api/Incident.ApiTests/IncidentControllerTest/GetIncidentByIdTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+using System.Linq;
+using Xunit;
 
 namespace Incident.ApiTests.IncidentControllerTest
 {
@@ -33,6 +35,37 @@ namespace Incident.ApiTests.IncidentControllerTest
             Assert.Equal(detail.Id.ToString(), incidentId);
             Assert.Equal(detail.DeviceDisplayName, device.DeviceDisplayName);
             Assert.Equal(detail.DeviceLocation, device.DeviceLocation);
+
+            await DeleteIncident(incidentId);
+            await DeleteWorkflowDesign();
+        }
+
+        [Fact]
+        public async void GetIncidentById_WithCamera_Success()
+        {
+            await ImportWorkflowDesign();
+
+            var getDeviceResponse = await IncidentGateWayApi.GetSiteDevicesAsync();
+            Assert.True(getDeviceResponse.IsSuccess);
+
+            var devices = getDeviceResponse.Value;
+            Assert.NotNull(devices);
+            Assert.True(devices.Length > 0);
+
+            var device = devices[0].Devices.FirstOrDefault(m=>m.DeviceDisplayName.EndsWith("_camera"));
+            Assert.NotNull(device);
+
+            var incidentId = await CreateIncident(device.DeviceId, device.DeviceType);
+
+            var getDetailResponse = await IncidentGateWayApi.GetDetailAsync(incidentId);
+
+            Assert.True(getDetailResponse.IsSuccess);
+            var detail = getDetailResponse.Value;
+            Assert.Equal(detail.Id.ToString(), incidentId);
+            Assert.Equal(detail.DeviceDisplayName, device.DeviceDisplayName);
+            Assert.Equal(detail.DeviceLocation, device.DeviceLocation);
+            Assert.False(string.IsNullOrEmpty(detail.CameraNumber));
+            Assert.Equal(detail.EventTimeStamp, new DateTimeOffset(detail.CreateAtUtc.Value).ToUnixTimeMilliseconds());
 
             await DeleteIncident(incidentId);
             await DeleteWorkflowDesign();
