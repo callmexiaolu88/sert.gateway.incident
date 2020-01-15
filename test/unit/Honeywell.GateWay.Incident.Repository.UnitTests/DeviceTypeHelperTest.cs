d510687d3384a9d767e903873457458bf7feec50
Baron Chen <Baron.Chen2@honeywell.com>
2020-01-15 14:50:48 +0800

    add test cases

---
+++ test/unit/Honeywell.GateWay.Incident.Repository.UnitTests/DeviceTypeHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Honeywell.GateWay.Incident.Repository.UnitTests
+{
+    public class DeviceTypeHelperTest
+    {
+        [Fact]
+        public void DeviceTypeHelperTest_Successful()
+        {
+            var type = "Door";
+            var formatType = DeviceTypeHelper.GetSystemDeviceType(type);
+            Assert.NotNull(formatType);
+            Assert.Equal("Prowatch_Door", formatType);
+        }
+    }
+}
