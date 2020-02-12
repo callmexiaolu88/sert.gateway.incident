f3c34cfd2a12033eb80edada9d8a55bd49f22adc
Baron Chen <Baron.Chen2@honeywell.com>
2020-02-12 14:28:20 +0800

    correct the get camera event time stamp issue.

--- test/api/Incident.ApiTests/IncidentControllerTest/GetIncidentByIdTest.cs
+++ test/api/Incident.ApiTests/IncidentControllerTest/GetIncidentByIdTest.cs
@@ -5,7 +5,7 @@ using Xunit;
 namespace Incident.ApiTests.IncidentControllerTest
 {
     [Collection(Colections.DICollection)]
-    public class GetIncidentByIdTest: BaseIncIdentControllerTest
+    public class GetIncidentByIdTest : BaseIncIdentControllerTest
     {
         public GetIncidentByIdTest(DIFixture dIFixture) : base(dIFixture)
         {
@@ -15,7 +15,7 @@ namespace Incident.ApiTests.IncidentControllerTest
         public async void GetIncidentById_WithDevice_Success()
         {
             await ImportWorkflowDesign();
-            
+
             var getDeviceResponse = await IncidentGateWayApi.GetSiteDevicesAsync();
             Assert.True(getDeviceResponse.IsSuccess);
 
@@ -52,12 +52,14 @@ namespace Incident.ApiTests.IncidentControllerTest
             Assert.NotNull(devices);
             Assert.True(devices.Length > 0);
 
-            var device = devices[0].Devices.FirstOrDefault(m=>m.DeviceDisplayName.EndsWith("_camera"));
+            var device = devices[0].Devices.FirstOrDefault(m => m.DeviceDisplayName.EndsWith("_camera"));
             Assert.NotNull(device);
 
             var incidentId = await CreateIncident(device.DeviceId, device.DeviceType);
 
             var getDetailResponse = await IncidentGateWayApi.GetDetailAsync(incidentId);
+
+
 
             Assert.True(getDetailResponse.IsSuccess);
             var detail = getDetailResponse.Value;
@@ -66,7 +68,10 @@ namespace Incident.ApiTests.IncidentControllerTest
             Assert.Equal(detail.DeviceDisplayName, device.DeviceDisplayName);
             Assert.Equal(detail.DeviceLocation, device.DeviceLocation);
             Assert.False(string.IsNullOrEmpty(detail.CameraNumber));
-            Assert.Equal(detail.EventTimeStamp, new DateTimeOffset(detail.CreateAtUtc.Value, TimeSpan.Zero).ToUnixTimeMilliseconds());
+            Assert.NotNull(detail.CreateAtUtc);
+            Assert.False(detail.CreateAtUtc.Value.Kind == DateTimeKind.Utc);
+            var utcCreateDateTime = DateTime.SpecifyKind(detail.CreateAtUtc.Value, DateTimeKind.Utc);
+            Assert.Equal(detail.EventTimeStamp, new DateTimeOffset(utcCreateDateTime).ToUnixTimeMilliseconds());
 
             await DeleteIncident(incidentId);
             await DeleteWorkflowDesign();
